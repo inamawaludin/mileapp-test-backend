83ebefc7d3b22fdabb41edd5e3558bf401357bd2
Muchammad In'am Awaludin <inam.awaludin@gmail.com>
2023-10-18 19:14:29 +0700

    Add mock data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Package;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        // Specify the path to your JSON file
+        $jsonFilePath = base_path("database/data/seed.json");
+
+        // Read and decode the JSON data
+        $data = json_decode(file_get_contents($jsonFilePath), true);
+
+        foreach ($data as $key => $value) {
+            Package::create($value);
+        }
     }
 }
